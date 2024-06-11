2ddbe42cad6cbed22188fedd2dabb1fa8e07963b
saksham.s <sakshams982@gmail.com>
2024-06-11 16:16:56 +0545

    modified the table component to implement sorting feature per column

--- CustomTable/src/app/resuable/table/table.component.ts
+++ CustomTable/src/app/resuable/table/table.component.ts
@@ -13,4 +13,30 @@ import {JsonPipe} from "@angular/common";
 export class TableComponent {
 @Input() headers:string[]=[];
 @Input() tableData:any[]=[];
+
+public orderBy='desc';
+
+
+
+onSort(event:Event,value:string,orderBy:string){
+const header=event.target as HTMLElement
+  const column:string=value;
+  const order:string=orderBy;
+
+  if(order=='desc'){
+    this.orderBy='asc'
+    this.tableData=this.tableData.sort((a,b):number  =>{
+      return a[column]>b[column]?1:-1;
+
+    })
+  }
+  else{
+    this.orderBy='desc'
+    this.tableData=this.tableData.sort((a,b):number  =>{
+      return a[column]<b[column]?1:-1;
+
+    })
+  }
+  console.log(column,order)
+}
 }
