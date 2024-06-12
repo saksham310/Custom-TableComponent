e5044a1f044e68fdedd87c57ae36628edf424f39
saksham.s <sakshams982@gmail.com>
2024-06-12 11:45:35 +0545

    implemented a basic pagination logic (incomplete)

--- CustomTable/src/app/resuable/table/table.component.ts
+++ CustomTable/src/app/resuable/table/table.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, ElementRef, Input, SimpleChange, ViewChild} from '@angular/core';
 import {JsonPipe} from "@angular/common";
 
 @Component({
@@ -13,22 +13,40 @@ import {JsonPipe} from "@angular/common";
 export class TableComponent {
   @Input() headers: string[] = [];
   @Input() tableData: any[] = [];
+  newTableData:any[]=[];
   public orderBy = 'desc';
+  icon='pi pi-sort-alt'
+  testIndex=1;
+@ViewChild('table') table!:ElementRef
 
 
+
+ngOnChanges(changes:SimpleChange){
+  if (changes.hasOwnProperty('tableData')) {
+    this.onPageChange(1)
+    this.onSort(this.headers[1],'desc')
+  }
+
+}
   onSort(value: string, orderType: string) {
     if(orderType=='desc'){
       this.orderBy='asc'
-      this.tableData=this.tableData.sort((a,b):number=>{
+      this.newTableData=this.newTableData.sort((a,b):number=>{
         return a[value]>b[value]?1:-1;
 
       })
     }
     else{
       this.orderBy='desc'
-      this.tableData=this.tableData.sort((a,b):number=>{
+      this.newTableData=this.newTableData.sort((a,b):number=>{
         return a[value]<b[value]?1:-1;
     })
 
   }
-}}
+}
+  onPageChange(start:number){
+  start=25*(start-1);
+  const end=start+25;
+    this.newTableData=this.tableData.slice(start,end)
+  }
+}
